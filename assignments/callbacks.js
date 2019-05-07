381cd13276559812428c7bc6b03b4bf58c6804d4
Ramses Bermudez <ramsesbermudez@Ramsess-MacBook-Pro.local>
2019-05-07 16:45:16 -0400

    began work on callbacks.js

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -1,4 +1,6 @@
-// Create a higher order function and invoke the callback function to test your work. You have been provided an example of a problem and a solution to see how this works with our items array.  Study both the problem and the solution to figure out the rest of the problems.
+// Create a higher order function and invoke the callback function to test your work. 
+// You have been provided an example of a problem and a solution to see how this works with our items array.
+// Study both the problem and the solution to figure out the rest of the problems.
 
 const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
@@ -24,18 +26,43 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 */
 
+// ------------------------------------------------------------------
+
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  return cb(arr.length);
 }
+
+getLength(items, function(length){
+console.log(length);
+});
+
+
+// ------------------------------------------------------------------
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  return cb(items[arr.length-1]);
 }
+
+last(items, function(lastItem){
+  console.log(lastItem);
+})
+
+// ------------------------------------------------------------------
+
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x * y);
 }
+
+sumNums(3, 3, function(product){
+  console.log(product);
+})
+
+// ------------------------------------------------------------------
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
